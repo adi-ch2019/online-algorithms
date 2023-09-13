250665abaddeac32b69954ba213b50f65921ddbe
adi-ch2019 <adi.ch2019@outlook.com>
2023-09-13 20:53:59 +0530

    replace 2nd string program

--- c-sharp/second-string-replace-digits-from-first/Program.cs
+++ c-sharp/second-string-replace-digits-from-first/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string N1="1234";
+string N2="1237";
+
+char[] charN1=N1.ToArray();
+char[] charN2 = N2.ToArray();
+
+int count=0;
+
+if(N1.Length==N2.Length && N1!=N2 )
+{
+    for(int i=N1.Length-1;i>=0;i--)
+    {
+        charN1[i]=charN2[i];
+        string? str1 = charN1.ToArray().ToString();
+        int intN1 =int.TryParse(charN1, out int result)?result:-1;
+       // Console.WriteLine(intN1);
+        if(intN1==Convert.ToInt16( N2))
+        {
+           
+        }
+        else
+        {
+            Console.WriteLine(charN1);
+            count++;
+        }
+    }
+}
+
+count++;
+
+Console.WriteLine("Count of numbers swapped of 1st string with the 2nd is :"+Convert.ToString(count) );
+
