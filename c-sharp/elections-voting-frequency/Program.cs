bdf8f60476dd195246921d5b32f5e5a9cd032de7
adi-ch2019 <adi.ch2019@outlook.com>
2023-09-21 18:39:33 +0530

    frequency of voting modulus

---
+++ c-sharp/elections-voting-frequency/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int inputAge =36;
+int inputVotingAge=18;
+int resultCount=0;
+int numYearsEachElection=4;
+
+if(inputAge>=inputVotingAge)
+{
+    for(int i=0;i<inputAge;i++)
+    {
+        if(i % numYearsEachElection ==0) resultCount++;
+    }
+
+    Console.WriteLine("Your voting frequency is "+resultCount);
+}
+else
+Console.WriteLine("Not eligible to vote you have [0] years to go",inputVotingAge-inputAge);
+
+Console.ReadKey();
