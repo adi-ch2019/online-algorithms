b10dc869aebcc7f9645f610ab18f9df271a9ae9d
adi-ch2019 <adi.ch2019@outlook.com>
2023-10-15 13:26:04 +0530

    initial commit

---
+++ c-sharp/csharp-interfaces/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomCollection.Tests
+{
+    public class StringMap<TValue> : IStringMap<TValue>
+        where TValue : class
+    {
+        private Dictionary<string, TValue> map = new Dictionary<string, TValue>();
+
+        public int Count => map.Count;
+
+        public TValue DefaultValue { get; set; }
+
+        public bool AddElement(string key, TValue value)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentException("Key cannot be an empty string", nameof(key));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (map.ContainsKey(key))
+            {
+                map[key] = value;
+                return true;
+            }
+
+            map[key] = value;
+            return false;
+        }
+
+        public bool RemoveElement(string key)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentException("Key cannot be an empty string", nameof(key));
+
+            return map.Remove(key);
+        }
+
+        public TValue GetValue(string key)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentException("Key cannot be an empty string", nameof(key));
+
+            if (map.TryGetValue(key, out TValue value))
+            {
+                return value;
+            }
+
+            return DefaultValue;
+        }
+    }
+}
