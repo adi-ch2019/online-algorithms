a3e45a1fb4750b5050b25aafae28f6c08c36c360
adi-ch2019 <adi.ch2019@outlook.com>
2023-11-09 01:14:19 +0530

    abstract classes sample

---
+++ c-sharp/abstractclasses-example/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var tCard=new TradingCard();
+
+        var theDerivedType=tCard.GetType();
+        bool isCorrectBaseType=theDerivedType.BaseType==typeof(Card);
+    }
+
+    public class Answer
+    {
+        public static bool ShowExectedResult=true;
+        public static bool ShowHints=true;
+    }
+
+    public class TradingCard:Card
+    {
+        public override global::System.Double GetSalePrice()
+        {
+            return 0;
+        }
+    }
+
+    public abstract class Card
+    {
+        public abstract double GetSalePrice();
+    }
+}
---
+++ c-sharp/abstractclasses-example/ReadMe.md
@@ -0,0 +1,7 @@
+# Implement Abstract Classes
+
+## The abstract **Card** class
+
+## Add abstract GetSalePrice method , return a double value
+
+### Inherit the **Card** class in your **TradingCard** class and override the GetSalePriceMethod
