b87739738390888178bd522d409d79dc8e3e79bc
adi-ch2019 <adi.ch2019@outlook.com>
2024-12-15 17:51:01 +0530

    letters with respective count in string

---
+++ c-sharp/letters-with-respective-counts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+string inputString="banana";
+IDictionary<char,int> charCount=new Dictionary<char,int>();
+List<string> outputParts = new List<string>();
+
+foreach(char c in inputString)
+{
+    if (charCount.ContainsKey(c))
+    {
+        charCount[c]++;
+    }
+    else
+    charCount.Add(c,1);
+     
+}
+foreach (KeyValuePair<char, int> kvp in charCount)
+{
+    outputParts.Add($"{kvp.Key}{kvp.Value}");
+}
+Console.WriteLine(string.Join("", outputParts));
