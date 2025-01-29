1c2e608ad7956eba2cfb08ace7ceb28be8507b04
adi-ch2019 <adi.ch2019@outlook.com>
2025-01-30 02:26:58 +0530

    remove duplicates and sort

---
+++ c-sharp/remove-duplicates-sort/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+IList<int> inputList=new List<int>{2,2,3,3,1,1};
+IList<int> outputList=new List<int>();
+foreach (int item in inputList)
+{
+    if (!outputList.Contains(item))
+    {
+        outputList.Add(item);
+
+    }
+}
+List<int> sortedList=new List<int>(outputList);
+sortedList.Sort();
+foreach(int item in sortedList)
+Console.WriteLine(item);
