b59c458cdcaa43250e0e80b6af066849b6cc55de
adi-ch2019 <adi.ch2019@outlook.com>
2026-02-12 07:59:08 +0530

    anagram check so we can validate the same

---
+++ c-sharp/strings/anagram/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program {
+
+    public static void Main(string[] args) {
+        string s = "anagram";
+        string t = "nagaramrrr";
+        Console.WriteLine(IsAnagram(s,t));
+    }       
+    public static bool IsAnagram(string s, string t) {
+        char[] sArray = s.ToArray();
+        char[] tArray = t.ToArray();
+        Array.Sort(sArray);
+        Array.Sort(tArray);
+       Console.WriteLine(sArray);
+       Console.WriteLine(tArray);
+        return new string(sArray) == new string(tArray);
+    }
+}
