c9516ba07f9c416705719ace6c8926acadd88c7f
adi-ch2019 <adi.ch2019@outlook.com>
2026-02-17 02:54:02 +0530

    duplicate check

---
+++ c-sharp/Arrays/duplicate-hashset/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program {
+    public static void Main(string[] args) {
+        int[] nums = {1, 2, 3, 1};
+        Program p = new Program();
+        bool result = p.ContainsDuplicate(nums);
+        Console.WriteLine(result); // Output: True
+    }
+    public bool ContainsDuplicate(int[] nums) {
+        HashSet<int> hsNums=new HashSet<int>();
+        
+        foreach(int i in nums)
+        {
+            if(!hsNums.Add(i)) return true;
+           
+        }    
+        return false;
+    }
+}
