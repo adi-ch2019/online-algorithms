3a7aa6265f869475e8800f260e2cfe89c4f30ad3
adi-ch2019 <adi.ch2019@outlook.com>
2026-02-20 17:53:24 +0530

    count primes

---
+++ c-sharp/math/count-primes/Program.cs
@@ -0,0 +1,29 @@
+﻿        int n=10;
+        if (n < 2) {
+            System.Environment.Exit(0);
+        }
+        
+        bool[] isPrime = new bool[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        
+        // Sieve of Eratosthenes
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        // Count primes
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) 
+            {
+                count++;
+                Console.WriteLine(i);
+            }
+        }
+        //Console.WriteLine(count);
