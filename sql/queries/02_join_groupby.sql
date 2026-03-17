e2f865ccb4276c23b5fcd27cb818ca943cf5fdfe
adi-ch2019 <adi.ch2019@outlook.com>
2026-03-18 03:28:29 +0530

    groupby count num for books

---
+++ sql/queries/02_join_groupby.sql
@@ -0,0 +1,9 @@
+-- Query 2: JOIN + GROUP BY
+-- Count books per author
+SELECT 
+    a.author_name as AuthorName,
+    COUNT(b.book_id) as BookCount
+FROM authors a
+LEFT JOIN books b ON a.author_id = b.author_id
+GROUP BY a.author_id, a.author_name
+ORDER BY BookCount DESC;
