2a8023f1b76d7da595c7325192229a4892e712fa
adi-ch2019 <58604572+adi-ch2019@users.noreply.github.com>
2026-03-24 21:36:04 +0000

    JOIN IS SAME AS LEFT JOIN

--- sql/queries/02_join_groupby.sql
+++ sql/queries/02_join_groupby.sql
@@ -1,7 +1,7 @@
 -- Query 5: With HAVING clause (authors with more than 1 book)
 SELECT a.author_name, COUNT(b.book_id) as book_count
 FROM authors a
-LEFT JOIN books b ON a.author_id = b.author_id
+JOIN books b ON a.author_id = b.author_id
 GROUP BY a.author_id, a.author_name
 -- HAVING COUNT(b.book_id) > 0
 HAVING COUNT(b.book_id) > 1
